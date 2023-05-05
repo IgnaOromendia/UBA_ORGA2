d581b6d2cdd6538c0c1e6fae71a6ec28be06f251
Ignacio Fernandez Oromendia <ignaciooromendia@gmail.com>
2023-05-05 12:17:32 -0300

    T4Offset

--- Talleres/Taller-4/src/filters/Offset_asm.asm
+++ Talleres/Taller-4/src/filters/Offset_asm.asm
@@ -1,6 +1,7 @@
 section .data
 
 max: db 255
+%define OFFSET_SIGUIENTES4PX 128
 
 section .text
 
@@ -16,21 +17,40 @@ Offset_asm:
 	push r15
 
 	; seteamos en 0
-	xor r12, r12	; iterador N
-	xor r13, r13	; iterador src
-	xor r14, r14	; iterador dst
-	xor r15, r15
+	xor r12, r12	; iterador i (Height)
+	xor r13, r13	; iterador j (Width)
+	xor r14, r14	; iterador src
+	xor r15, r15	; iterador dst
 	pxor xmm0, xmm0 ; pixeles src
+	pxor xmm1, xmm1 ; pixeles dst
 
 	; seteamos valores
-	mov r13, [rdi]
-	mov r14, [rsi] 
+	mov r14, [rdi]
+	mov r15, [rsi] 
 
-	.ciclo: 
+	.cicloH: 
+		cmp r12, rdx	; if (i == height) 
+		je .fin
+		.cicloW:
+			cmp r13, rcx		; if (j == width)
+			je .finW
+			inc r13				; j++
+			movdqu xmm0, [r14]	; cargo 4 pixeles de src
+			cmp r12, 8			; if (i < 8)
+			jl .aplicarMarco
+			cmp r13, 0			; if (j == 0)
+			je .aplicarMarco
+			cmp r13, 
+
+			.aplicarMarco:
+
+
+		.finW:
+		xor r13, r13 	; j = 0
+		inc r12			; i++
+		jmp .cicloH		
 		
-
-
-
+	.fin:
 	pop r15
 	pop r14
 	pop r13
