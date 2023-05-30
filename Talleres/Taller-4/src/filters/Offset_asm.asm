4200659c2440b6b06431aa7364c5bec19328c729
Ignacio Fernandez Oromendia <ignaciooromendia@gmail.com>
2023-05-30 16:03:51 -0300

    Offset

--- Talleres/Taller-4/src/filters/Offset_asm.asm
+++ Talleres/Taller-4/src/filters/Offset_asm.asm
@@ -1,3 +1,5 @@
+global Offset_asm
+
 section .data
     black:  db 0,0,0,255,0,0,0,255,0,0,0,255,0,0,0,255
     blue:   db 255,0,0,0,255,0,0,0,255,0,0,0,255,0,0,0
@@ -16,8 +18,6 @@ section .text
 ; python3 2_test_diff_cat_asm.py
 ; ./3_correr_test_mem.sh 
 
-global Offset_asm
-
 ; void Offset_asm (uint8_t *src, uint8_t *dst, int width, int height, int src_row_size, int dst_row_size);
 ; rdi = *src, rsi = *dst, rdx = width, rcx = height, r8 = src_row_size, r9 = dst_row_size
 Offset_asm:
@@ -25,83 +25,92 @@ Offset_asm:
     push rbp
     mov rbp,rsp
 
-    movdqu xmm4, [black]    ; creo 4px negros(0,0,0,255)
-    xor r10, r10            ; seteo en 0
-    shl r9, 3               ; en r9 nos queda el row_size * 8, esto es asi para facilidad de implementacion
+    push r12
+    push r13
+
+    movdqu xmm4, [black]    ; creo 2PX negros(0,0,0,255)
+    xor r10, r10            ; seteo en 0 - iterador de dst
+    shl r9, 3               ; en r9 nos queda el row_size * 8, para ir recorriendolo
     sub rdx, 16             ; resto los bordes negros
     sub rcx, 16             ; resto los bordes negros
 
     ; hago las primeras 8 filas negras
-    xor rax, rax                    ; hacemos rax = 0
+    xor rax, rax                    ; seteo rax = 0
     loop_top:
-        movaps [rsi + r10], xmm4    ; cargamos 4px negros
-        add r10, 16                 ; avanzamos 16bytes (4 pixeles)
-        add rax, 2
-        cmp rax, r8
+        movaps [rsi + r10], xmm4    ; cargamos 2PX negros
+        add r10, 16                 ; avanzamos 2PX
+        add rax, 2                  ; rax += 2
+        cmp rax, r8                 ; if (rax == src_row_size)
         jne loop_top                ; volvemos a loopear si no son iguales
 
-    ; hacemos la imagen del medio
-    push r12
-    xor r12, r12
-    loop_shuffle:
-        ; primero 8 px negros
-        movaps [rsi + r10], xmm4
-        add r10, 16
-        movaps [rsi + r10], xmm4
-        add r10, 16
 
-        ; ahora hago el shuffle
-        xor rax, rax ; seteo en 0
-        loop_shuffle_int:
+    ; Imagen del medio
+    xor r12, r12
+    loop_offset:
+        ; primero 8 px negros
+        movaps [rsi + r10], xmm4        ; aplico los primeros 2PX
+        add r10, 16                     ; avanzo 2PX
+        movaps [rsi + r10], xmm4        ; aplico los segundos 2PX
+        add r10, 16                     ; avanzo 2PX
+
+        
+        xor rax, rax ; seteo en 0 - contador de pixeles
+        loop_offset_int:
             
             ; marco los pixeles que voy a pintar
-            movdqu xmm0, [black]            ; cargamos el valor black en xmm0
-            movdqa xmm2, [rdi + r10 + 16*2] ; guardamos en xmm2 los 4px que estan a 4px de distancia de los de xmm0 por los bordes
-            mov r11, r10
-            add r11, r9
-            movdqa xmm1, [rdi + r11]        ; guardamos en xmm1 los 4px que estan 8 mas abajo del que vamos a pintar
-            add r11, 32
-            movdqa xmm3, [rdi + r11]
+            movdqu xmm0, [black]            ; cargo el valor black en xmm0
+            movdqa xmm2, [rdi + r10 + 32]   ; cargo en xmm2 img[i][j+8]
+            mov r11, r10                    ; r11 = img[i][j]
+            add r11, r9                     ; r11 = img[i+8][j]
+            movdqa xmm1, [rdi + r11]        ; cargo en xmm1 los 2PX que estan 8 mas abajo del que vamos a pintar
+            add r11, 32                     ; r11 = img[i+8][j+8]
+            movdqa xmm3, [rdi + r11]        ; cargo en xmm3 los 2PX de la diagonal
 
-            ; "filtramos" los colores que nos importa de cada set de 4pixeles
-            movdqu xmm5, [blue]             ; guardamos el valor de blue en xmm5 temporalmente
-            pand xmm1, xmm5
-            movdqu xmm5, [green]
+            ; Filtro colores
+            ; blue
+            movdqu xmm5, [blue]             ; xmm5 = blue
+            pand xmm1, xmm5                 
+            ; green
+            movdqu xmm5, [green]            ; xmm5 = green
             pand xmm2, xmm5
-            movdqu xmm5, [red]
+            ; red
+            movdqu xmm5, [red]              ; xmm3 = red
             pand xmm3, xmm5
-            ; combinamos los valores en xmm0
+
+            ; combino los valores en xmm0
             por xmm0, xmm1
             por xmm0, xmm2
             por xmm0, xmm3
-            ; guardamos el valor de xmm0 en la imagen dst
+
+            ; guatdo en dst
             movaps [rsi + r10], xmm0
             add r10, 16
 
-            add rax, 4
-            cmp rax, rdx
-            jne loop_shuffle_int
+            add rax, 4                  ; rax += 4
+            cmp rax, rdx                ; if (rax == width)
+            jne loop_offset_int
 
         ; proximos 8 px negros
-        movaps [rsi + r10], xmm4
-        add r10, 16
-        movaps [rsi + r10], xmm4
-        add r10, 16
+        movaps [rsi + r10], xmm4        ; aplico los primeros 2PX
+        add r10, 16                     ; avanzo 2PX
+        movaps [rsi + r10], xmm4        ; aplico los segundos 2PX
+        add r10, 16                     ; avanzo 2PX
 
-        inc r12     ; incrementamos en 1 el r12 por cada linea que avanzamos
-        cmp r12, rcx
-        jne loop_shuffle
-
-    pop r12
+        inc r12                         ; r12++
+        cmp r12, rcx                    ; if (r12 == height)
+        jne loop_offset
 
     ; hago las ultimas 8 filas negras
     xor rax, rax        ; hacemos rax = 0
     loop_end:
-        movaps [rsi + r10], xmm4 ; cargamos 4px negros
-        add r10, 16     ; avanzamos 16bytes (4 pixeles)
-        add rax, 2
-        cmp rax, r8
-        jne loop_end    ; volvemos a loopear si no son iguales
+        movaps [rsi + r10], xmm4    ; cargamos 2PX negros
+        add r10, 16                 ; avanzamos 2PX
+        add rax, 2                  ; rax += 2
+        cmp rax, r8                 ; if (rax == src_row_size)
+        jne loop_end                ; volvemos a loopear si no son iguales
+
+    pop r13
+    pop r12
 
     ;epilogo
     pop rbp
