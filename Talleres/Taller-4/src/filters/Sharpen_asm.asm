db6f58bb5566cfc01c7d650e693dd7605a1269df
Ignacio Fernandez Oromendia <ignaciooromendia@gmail.com>
2023-06-05 16:15:38 -0300

    Sharpen

--- Talleres/Taller-4/src/filters/Sharpen_asm.asm
+++ Talleres/Taller-4/src/filters/Sharpen_asm.asm
@@ -1,6 +1,7 @@
 section .data
-    marco:      db 0,0,0,255,0,0,0,255,0,0,0,255,0,0,0,255
-    sharpen: 
+    black:      db 0,0,0,255,0,0,0,255,0,0,0,255,0,0,0,255
+    cut:        db 255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255
+    take_bor:   db 255,255,255,255,0,0,0,0,0,0,0,0,255,255,255,255
 
 section .text
 
@@ -19,46 +20,194 @@ global Sharpen_asm
 ; void Sharpen_asm (uint8_t *src, uint8_t *dst, int width, int height, int src_row_size, int dst_row_size);
 ; rdi = *src, rsi = *dst, rdx = width, rcx = height, r8 = src_row_size, r9 = dst_row_size
 Sharpen_asm:
+    ;prologo
     push rbp
     mov rbp,rsp
 
+    ; acomodo los registros que utilizare durante el programa
+    movdqu xmm6, [black]    ; crear marco
+    movdqu xmm7, [take_bor] ; xmm7 -> [ff,0,0,ff] (en realidad son mas ff pero se entiende que hace) ignorar los medio
+    sub rdx, 2              ; estamos restando los bordes negros al "tamaño final"
+    sub rcx, 2              ; estamos restando los bordes negros al "tamaño final"
+
+    ; mascaras
+    ; xmm10 -> [0,0,0,0,ff,ff,ff,ff]
+    movdqu xmm10, [cut]
+    pslldq xmm10, 8
+    ; xmm11 -> [ff,ff,ff,ff,0,0,0,0]
+    movdqu xmm11, [cut]
+    psrldq xmm11, 8
+    ; xmm12 -> [0,0,0,ff,ff,...,ff]
+    movdqu xmm12, [cut]
+    pslldq xmm12, 3
+    ; xmm13 -> [ff,ff,...,0,0,0,ff]
+    movdqu xmm13, [cut]
+    psrldq xmm13, 4
+    por xmm13, xmm6
+
+    ; hacemos la  linea superior
+    xor r10, r10
+    top_black:
+        movups [rsi + r10], xmm6
+        add r10, 4*4                ; 4(px) * 4(bytes cada px)
+        cmp r10, r8
+        jne top_black
+
     push r12
-    push r13
-    push r14
-    push r15
+    push rdi
+    push rsi
+    xor r12, r12
+    loop_sharpen:
+        xor r10, r10            ; este registro guardara el avance por linea en px
+        ; pintamos el primer px negro
+        movdqu xmm0, [rsi + r8]
+        pand xmm0, xmm12
+        movups [rsi + r8], xmm0
+        loop_line:
+            mov r11, r10
+            shl r11, 2                  ; multiplicamos por 4 = 2^2
 
-    movdqu xmm0, [marco]    ; cargo el marco
-    sub rdx, 2              ; resto los bordes
-    sub rcx, 2              ; resto los brodes
+            ; tomo los 4 pixeles de arriba
+            ; queremos que xmm0 sea = [b0,g0,r0,a0,b1,g1,r1,a1] y xmm1 los proximos 2 pixeles
+            movdqu xmm0, [rdi + r11]    ; tomamos 4 px de 8bits cada valor del mismo
+            movdqu xmm1, xmm0           ; copiamos a xmm1
+            ; las siguientes dos lineas no son necesarias
+            ;pslldq xmm0, 8              ; movemos 8bytes(64bits) a la derecha -> [0,0,px0,px1]
+            ;psrldq xmm0, 8              ; movemos ahora los 2 px que quedaron a la posicion mas significativa <- [px0,px1,0,0]
+            pmovzxbw xmm0, xmm0         ; extendemos de 8 a 16
+            psrldq xmm1, 8              ; movemos de derecha a izq <- [px2,px3,0,0]
+            pmovzxbw xmm1, xmm1         ; extendemos
 
-    xor r10, r10            ; iterador
-    marco_superior:
-        movups [rsi + r10], xmm0    ; aplico marco
-        add r10, 16                 ; avanzo 4px
-        cmp r10, r8                 ; if (i == row_size )
-        jne  marco_superior
+            ; repetimos para los 4 px del medio
+            ; como solo podemos usar dos registros en [] copiamos r10 a r11 y le agregamos r8
+            add r11, r8
+            movdqu xmm2, [rdi + r11]
+            movdqu xmm3, xmm2
+            ; las siguientes dos lineas no son necesarias
+            pslldq xmm2, 8
+            psrldq xmm2, 8
+            pmovzxbw xmm2, xmm2
+            psrldq xmm3, 8
+            pmovzxbw xmm3, xmm3
 
-    
-    mov r12, r8                 ; iterador columna
-    inc r12                     ; empiezo en la segunda columna porque la primera tiene marco
-    xor r13, r13                ; iterador matriz sharpen
-    sharpen:
-        xor r10, r10            ; iterador de fila
-        movups [rsi + r8], xmm0 ; aplico el marco de la primer columna
+            ; repetimos para los 4 px de abajo
+            add r11, r8
+            movdqu xmm4, [rdi + r11]
+            movdqu xmm5, xmm4
+            ; las siguientes dos lineas no son necesarias
+            pslldq xmm4, 8
+            psrldq xmm4, 8
+            pmovzxbw xmm4, xmm4
+            psrldq xmm5, 8
+            pmovzxbw xmm5, xmm5
 
-        sharpen_interno:
-            movd r14, [rsi + r12 + r10]     ; cargo 1 pixel
-            movdqu xmm1, [rsi + r12 + r11]  ; cargo 4 pixeles [px1,px2,px3,pxZ] siendo pxZ uno que no me sirve
-            pslldq xmm1, 4                  ; me deshago del que no me sirve [0,px1,px2,px3]
-            
+            ; xmm0 xmm1
+            ; xmm2 xmm3
+            ; xmm4 xmm5
 
+            ; px0  px1  px2  px3
+            ; px4  px5  px6  px7
+            ; px8  px9  px10 px11
 
-            
+            ; para reducir restas a futuro
+            ; sumamos xmm0 con xmm4 y xmm1 con xmm5
+            paddw xmm0, xmm4
+            paddw xmm1, xmm5
 
+            ; en este punto tendriamos:
+            ; |     xmm0    |       xmm1      |
+            ; | p0+p8,p1+p9 | px2+p10,px3+p11 |
+            ; |     xmm2    |       xmm3      |
+            ; |    p4, p5   |      p6, p7     |
+            ; estos de que deberian estar ya no importan xq estan arriba
 
-    pop r15
-    pop r14
-    pop r13
+            ; pasamos xmm2 y xmm3 a xmm4 y 5 respectivamente
+            movdqu xmm4, xmm2
+            movdqu xmm5, xmm3
+
+            ; |     xmm0    |       xmm1      |
+            ; | p0+p8,p1+p9 | px2+p10,px3+p11 |
+            ; |     xmm2    |       xmm3      |
+            ; |    p4, p5   |      p6, p7     |
+            ; |     xmm4    |       xmm5      |
+            ; |    p4, p5   |      p6, p7     |
+
+            ; multiplicamos xmm4 y xmm5 por 9
+            psllw xmm4, 3       ; multiplico por 2^4=8
+            paddw xmm4, xmm2    ; le sumamos 1 => 8+1=9
+            psllw xmm5, 3
+            paddw xmm5, xmm3
+
+            ; aplicamos mascara
+            ; cortamos los pixeles no deseados
+            pand xmm4, xmm10
+            pand xmm5, xmm11
+
+            ; |     xmm0    |      xmm1     |
+            ; | p0+p8,p1+p9 | p2+p10,p3+p11 |
+            ; |     xmm2    |      xmm3     |
+            ; |    p4, p5   |     p6, p7    |
+            ; |     xmm4    |      xmm5     |
+            ; |    0,p5*9   |     p6*9,0    |
+
+            psubsw xmm4, xmm0   ; xmm4-r(xmm0) p5 - (p1 + p9)
+            psubsw xmm5, xmm1   ; xmm5-l(xmm1) p6 - (p2 + p10)
+
+            psrldq xmm4, 8      ; desplazamos xmm4 hacia la izq
+            pslldq xmm5, 8      ; desplazamos xmm5 hacia la derecha
+
+            psubsw xmm4, xmm0   ; xmm4-l(xmm0) p5 - (p0 + p8)
+            psubsw xmm5, xmm1   ; xmm5-r(xmm1) p6 - (p2 + p10)
+
+            psubsw xmm4, xmm2   ; xmm4-l(xmm2) p5 - p4
+            psubsw xmm5, xmm3   ; xmm5-r(xmm3) p5 - p7
+
+            psubsw xmm4, xmm1   ; xmm4-l(xmm1) p5 - (p2 + p10)
+            psubsw xmm5, xmm0   ; xmm4-r(xmm0) p5 - (p1 + p9)
+
+            psubsw xmm4, xmm3   ; xmm4-l(xmm3) p5 - p6
+            psubsw xmm5, xmm2   ; xmm4-r(xmm2) p6 - p5
+
+            ; volvemos a acomadar
+            pslldq xmm4, 8      ; desplazamos xmm4 hacia la derecha
+            psrldq xmm5, 8      ; desplazamos xmm5 hacia la izq
+
+            packuswb xmm4, xmm5         ; unimos xmm4 y xmm5 y convertimos los valores de 16 en 8bits con sat [0,sh1,sh2,0]
+            sub r11, r8                 ; volves a apuntar donde estabas
+
+            movdqu xmm0, [rsi + r11]    ; tomo 4px (donde guardare los del medio)
+            pand xmm0, xmm7             ; paso xmm0 por la mascara take_bor (me deja los 2 px del borde)
+            por xmm4, xmm0              ; en xmm4 queda lo que arme + los px laterales previos
+            movups [rsi + r11], xmm4    ; guardamos xmm4 en la imagen destino
+
+            add r10, 2                  ; avanzamos r10 (el contador de avance en px)
+            cmp r10, rdx
+            jne loop_line
+
+        ; pintamos el ultimo pixel de negro
+        movdqu xmm0, [rsi + r11]
+        pand xmm0, xmm13
+        movups [rsi + r11], xmm0
+
+        add rdi, r8
+        add rsi, r8
+        inc r12             ; avance de una linea
+        cmp r12, rcx
+        jne loop_sharpen
+
+    ; hacemos la  linea inferior
+    add rsi, r8
+    xor r10, r10
+    btm_black:
+        movups [rsi + r10], xmm6
+        add r10, 4*4                ; 4(px) * 4(bytes cada px)
+        cmp r10, r8
+        jne btm_black
+
+    pop rsi
+    pop rdi
     pop r12
+
+    ;epilogo
     pop rbp
 	ret
